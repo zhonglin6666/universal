d64a38fe04e5a56dc9463ee09b465e00ab0d994a
zhangzhonglin <405358903@qq.com>
2016-10-10 17:58:57 +0800

    add api file

---
+++ api/handler.go
@@ -0,0 +1,45 @@
+package api
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/ngaut/log"
+)
+
+const (
+	RVERSION = "/v1/"
+)
+
+var MasterRouter = map[string]map[string]func(http.ResponseWriter, *http.Request){
+	"GET": {
+		"db/test": getTest,
+	},
+
+	"POST": {},
+
+	"DELETE": {},
+}
+
+func HandlerRouter(mRouter map[string]map[string]func(http.ResponseWriter, *http.Request)) http.Handler {
+	router := mux.NewRouter()
+
+	for method, routes := range mRouter {
+		for route, fct := range routes {
+			localRoute := RVERSION + route
+			localMethod := method
+			handler := handlerRequest(fct)
+			router.Path(localRoute).Methods(localMethod).HandlerFunc(handler)
+		}
+	}
+	router.NotFoundHandler = http.NotFoundHandler()
+
+	return router
+}
+
+func handlerRequest(handlerFunc func(http.ResponseWriter, *http.Request)) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Info("server API handRequest: %s", r.URL)
+		handlerFunc(w, r)
+	}
+}
